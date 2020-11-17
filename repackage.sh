d7c11eeeee3380d50d2b7eebf392c22279a2e608
Alex Dolski <alexd@n-ary.net>
2020-11-17 12:40:09 -0600

    Don't require a version argument

--- repackage.sh
+++ repackage.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 
-if [ $# -eq 0 ]
-  then
-    echo "Usage: repackage.sh <cantaloupe version>"
-    exit -1
-fi
-
 CWD=$(pwd)
 cd ../../cantaloupe-project/cantaloupe
 mvn clean package -DskipTests
 cd $CWD
 rm ./image_files/cantaloupe-*.zip
 cp ../../cantaloupe-project/cantaloupe/target/cantaloupe-*.zip ./image_files
-./docker-build.sh
